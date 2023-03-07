a61d118cd51b2796f2a0dfa417847514e992235e
Plamenpowa <pkostadinov72@gmail.com>
2023-03-08 01:09:49 +0200

    feat{Quizz, Answers} code restructure , some functionallity change.

--- QuizApp/src/components/Answers.vue
+++ QuizApp/src/components/Answers.vue
@@ -1,9 +1,12 @@
 <template>
-  <button v-for="item in answers">{{ item }}</button>
+  <button v-for="item in answers" @click="emits('submitAnswer', item)">
+    {{ item }}
+  </button>
 </template>
 
 <script setup lang="ts">
 const props = defineProps(["answers"]);
+const emits = defineEmits(["submitAnswer"]);
 </script>
 
 <style></style>
