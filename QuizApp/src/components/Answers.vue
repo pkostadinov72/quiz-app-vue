0eb3d6f541b4218ec0af05bad638724ee484b1af
Plamenpowa <pkostadinov72@gmail.com>
2023-03-08 10:46:26 +0200

    feat{Question,Answers} some css styling

--- QuizApp/src/components/Answers.vue
+++ QuizApp/src/components/Answers.vue
@@ -1,12 +1,44 @@
 <template>
-  <button v-for="item in answers" @click="emits('submitAnswer', item)">
+  <button
+    class="btnAnswer"
+    v-for="item in answers"
+    @click="emits('submitAnswer', item)"
+  >
     {{ item }}
   </button>
 </template>
 
 <script setup lang="ts">
 const props = defineProps(["answers"]);
-const emits = defineEmits(["submitAnswer"]);
+const emits = defineEmits(["submitAnswer", "changeStyle"]);
 </script>
 
-<style></style>
+<style>
+.btnAnswer {
+  display: inline-block;
+  outline: 0;
+  cursor: pointer;
+  text-align: center;
+  border: 1px solid #babfc3;
+  padding: 7px 16px;
+  margin: 8px;
+  min-height: 36px;
+  min-width: 36px;
+  color: #202223;
+  background: #ffffff;
+  border-radius: 4px;
+  font-weight: 500;
+  font-size: 14px;
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 0px 0px;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+}
+
+.correct {
+  background: #00ff77;
+}
+
+.wrong {
+  background: #ed0739;
+}
+</style>
--- QuizApp/src/components/Question.vue
+++ QuizApp/src/components/Question.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>{{ question }}</div>
+  <div class="questionText">{{ question }}</div>
 </template>
 
 <script setup lang="ts">
 const props = defineProps(["question"]);
 </script>
 
-<style></style>
+<style>
+.questionText {
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+  font-size: 1.5rem;
+  margin: 10px;
+}
+</style>
